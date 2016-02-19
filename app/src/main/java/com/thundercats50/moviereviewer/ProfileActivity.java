af0d8003d4edcdbc1bd2e77b2955d411fca5af56
nbarooah3 <neilbarooah@lawn-143-215-94-65.lawn.gatech.edu>
2016-02-19 02:46:55 -0500

    Updated ProfileActivity class for updating password

--- app/src/main/java/com/thundercats50/moviereviewer/ProfileActivity.java
+++ app/src/main/java/com/thundercats50/moviereviewer/ProfileActivity.java
@@ -5,6 +5,7 @@ import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
 import android.content.Intent;
@@ -13,12 +14,15 @@ import com.thundercats50.moviereviewer.models.MemberManager;
 import com.thundercats50.moviereviewer.models.Member;
 import com.thundercats50.moviereviewer.models.User;
 
+import java.sql.SQLException;
+
 public class ProfileActivity extends AppCompatActivity {
 
     private EditText name;
     private EditText email;
     private EditText gender;
     private EditText major;
+    private EditText password;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -29,6 +33,7 @@ public class ProfileActivity extends AppCompatActivity {
 
     }
 
+
     public void updateProfile(View view) {
         MemberManager manager = (MemberManager) getApplicationContext();
         User member = (User) manager.getCurrentMember();
@@ -36,12 +41,65 @@ public class ProfileActivity extends AppCompatActivity {
         email = (EditText) findViewById(R.id.editEmail);
         gender = (EditText) findViewById(R.id.editGender);
         major = (EditText) findViewById(R.id.editMajor);
+        password = (EditText) findViewById(R.id.editPassword);
         member.setFirstname(name.getText().toString());
         member.setEmail(email.getText().toString());
         member.setGender(gender.getText().toString());
         member.setMajor(major.getText().toString());
-        manager.updateMember(manager.getCurrentEmail(), member);
-        startActivity(new Intent(this, LoggedInActivity.class));
+        String passwordInput = password.getText().toString();
+        if (passwordInput.equals("")) {
+            startActivity(new Intent(this, LoggedInActivity.class));
+        } else  {
+            View focusView =  null;
+            boolean cancel = false;
+            if (!isPasswordValid(passwordInput)) {
+                password.setError("Please input a 6 character alphanumeric password");
+                focusView = password;
+                cancel = true;
+            }
+            if (cancel) {
+                focusView.requestFocus();
+            } else {
+                try {
+                    DBConnector dbc = new DBConnector();
+                    boolean retVal = dbc.changePass(member.getUsername(), passwordInput);
+                    Log.d("DB changePass Finished", "updateProfile method returned: "
+                            + Boolean.toString(retVal));
+                    dbc.disconnect();
+                    member.setPassword(passwordInput);
+                } catch (ClassNotFoundException cnfe) {
+                    Log.d("Dependency Error", "Check if MySQL library is present.");
+                } catch (SQLException sqle) {
+                    Log.d("Connection Error", "Check internet for MySQL access." + sqle.getMessage() + sqle.getSQLState());
+                    for (Throwable e : sqle) {
+                        e.printStackTrace(System.err);
+                        Log.d("Connection Error", "SQLState: " +
+                                ((SQLException) e).getSQLState());
+
+                        Log.d("Connection Error", "Error Code: " +
+                                ((SQLException) e).getErrorCode());
+
+                        Log.d("Connection Error", "Message: " + e.getMessage());
+
+                        Throwable t = sqle.getCause();
+                        while(t != null) {
+                            Log.d("Connection Error", "Cause: " + t);
+                            t = t.getCause();
+                        }
+                    }
+                }
+                startActivity(new Intent(this, LoggedInActivity.class));
+            }
+        }
+    }
+
+    /**
+     * Uses RegEx to verify pass; must be more than 6 chars and alphnumeric
+     * @param password
+     * @return ifValid
+     */
+    private boolean isPasswordValid(String password) {
+        return (password.matches("[a-zA-Z0-9]{6,30}") && password.length() > 6);
     }
 
     public void cancel(View view) {
