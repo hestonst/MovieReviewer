5abe330b3a99f5e068dde3458c7cc5204e6c8cc8
Scott Heston <sheston@lawn-128-61-38-7.lawn.gatech.edu>
2016-03-13 21:59:06 -0400

    Made RepoConnecter automatically overwrite user's old reviews for editting

--- app/src/main/java/com/thundercats50/moviereviewer/database/RepositoryConnector.java
+++ app/src/main/java/com/thundercats50/moviereviewer/database/RepositoryConnector.java
@@ -47,9 +47,6 @@ public class RepositoryConnector extends DBConnector {
     }
 
 
-    //TODO: write method deal with overwritting
-    //
-
     /**
      * method to query DB for ratings matching major
      * @param major to search for ratings
@@ -146,6 +143,37 @@ public class RepositoryConnector extends DBConnector {
     }
 
 
+
+    /**
+     * method to query DB for ratings matching username
+     * @param email to search for ratings
+     * @param id to match
+     * @return ResultSet (which can be accessed through a for-while loop)
+     */
+    public ResultSet getRating(String email, double id) {
+        ResultSet resultSet = null;
+        try {
+            if (connection == null) connect();
+            statement = connection.createStatement();
+            //keep making new statements as security method to keep buggy code from accessing
+            // old data
+            String request = "SELECT MovieID,NumericalRating," +
+                    "TextReview, PhotoURL FROM sql5107476.RatingInfo WHERE Email="
+                    + "'" + email +"' AND MovieID =" + Double.toString((double) id)
+                    + " ORDER BY NumericalRating";
+            resultSet = statement.executeQuery(request);
+        } catch (SQLException sqle) {
+            Log.e("Database SQLException", sqle.getMessage());
+            Log.e("Database SQLState", sqle.getSQLState());
+            Log.e("Database VendorError", Integer.toString(sqle.getErrorCode()));
+        }
+        return resultSet;
+    }
+
+
+
+
+
     /**
      * Method to add review to database. Screens info to prevent duplicates.
      * @param email email of current user
@@ -156,6 +184,7 @@ public class RepositoryConnector extends DBConnector {
                              String textReview)
             throws SQLException, InputMismatchException {
         ResultSet resultSet = null;
+        String request = "";
         String photoURL = movie.getThumbnailURL();
         String movieName = movie.getTitle();
         String synopsis = movie.getSynopsis();
@@ -165,10 +194,16 @@ public class RepositoryConnector extends DBConnector {
                 throw new InputMismatchException("Rating must be from 1-100");
             }
             statement = connection.createStatement();
-            String request = "INSERT INTO sql5107476.RatingInfo (MovieID, MovieName, Synopsis, " +
-                    "PhotoURL, Email, NumericalRating, TextReview) VALUES ("
-                    + movieID + ",'" + movieName + "','" + synopsis + "','" + photoURL + "','"
-                    + email + "'," + numericalRating + "','" + textReview + "')";
+            ResultSet previous = getRating(email, movieID);
+            if (previous.next()) {
+                request = "UPDATE sql5107476.RatingInfo SET NumericalRating=" + numericalRating
+                        + ", TextReview='" + textReview + "'";
+            } else {
+                request = "INSERT INTO sql5107476.RatingInfo (MovieID, MovieName, Synopsis, " +
+                        "PhotoURL, Email, NumericalRating, TextReview) VALUES ("
+                        + movieID + ",'" + movieName + "','" + synopsis + "','" + photoURL + "','"
+                        + email + "'," + numericalRating + "','" + textReview + "')";
+            }
 
             statement.executeUpdate(request);
         }
