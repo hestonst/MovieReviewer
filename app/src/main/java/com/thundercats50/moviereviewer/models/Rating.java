984fef06e468f3af8ddb3c35e254bcaf43b30525
Forester Vosburgh <fvosburgh3@gmail.com>
2016-04-11 14:39:45 -0400

    reverted before lint/models merge

--- app/src/main/java/com/thundercats50/moviereviewer/models/Rating.java
+++ app/src/main/java/com/thundercats50/moviereviewer/models/Rating.java
@@ -16,18 +16,36 @@ public class Rating {
         this.user = user;
     }
 
+    /**
+     * Get the text review of the current movie
+     * @return a string that is the text review
+     */
     public String getTextReview() {
         return textReview;
     }
 
+
+    /**
+     * sets the text review for the current movie
+     * @param textReview the review
+     */
     public void setTextReview(String textReview) {
         this.textReview = textReview;
     }
 
+
+    /**
+     * get the numerical rating fot the current movie
+     * @return the numerical rating
+     */
     public int getNumericalRating() {
         return numericalRating;
     }
 
+    /**
+     * set the current movie's numerical rating
+     * @param numericalRating the rating
+     */
     public void setNumericalRating(int numericalRating) {
         this.numericalRating = numericalRating;
     }
--- app/src/main/java/com/thundercats50/moviereviewer/models/SingleMovie.java
+++ app/src/main/java/com/thundercats50/moviereviewer/models/SingleMovie.java
@@ -24,18 +24,36 @@ public class SingleMovie {
     private Integer year, runtime;
     private HashMap<String, Rating> userRatings;
 
+    /**
+     * Create an empty movie object
+     */
     public SingleMovie() {
         userRatings = new HashMap<>();
     }
 
+    /**
+     * get the rating of this movie based on the user
+     * @param email the users email
+     * @return the rating that user made
+     */
     public int getUserRating(String email) {
         return userRatings.get(email).getNumericalRating();
     }
 
+    /**
+     * see if a user made a rating
+     * @param email the users email
+     * @return true if the user made a rating for this movie, false otherwise
+     */
     public boolean hasRatingByUser(String email) {
         return userRatings.containsKey(email);
     }
 
+    /**
+     * get the review a user
+     * @param email the users email
+     * @return the review
+     */
     public String getUserReview(String email) {
         return userRatings.get(email).getTextReview();
     }
